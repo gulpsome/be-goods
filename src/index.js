0f12556ff20a83c7320ce3344af6408d2a176b8b
Orlin M Bozhinov <om@soundsapiens.com>
2015-06-13 18:16:27 +0300

    harp pollinator

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 require('source-map-support').install()
+
 import path from 'path'
 import sourcegate from 'sourcegate'
 let flaments = require(path.normalize('../src/pollen.json'))
@@ -8,4 +9,14 @@ export function pollen(anthers) {
     return typeof select === 'string' ? flaments[select] : select // object assumed
   })
   return sourcegate(got)
+}
+
+export function pollinateHarp(o) {
+  if (!o.harp) return {}
+  // infer what pollen is wanted
+  let anthers = ['harp']
+  if (o.harp.sync) anthers.push('harp-sync')
+  anthers.push(o)
+  // harp options
+  return pollen(anthers)
 }
