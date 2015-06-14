e78bfeb8705599fe52bf81eed3ccd3115311c7ef
Orlin M Bozhinov <om@soundsapiens.com>
2015-06-14 20:02:04 +0300

    export pkg and gulpIsHelpful

--- src/index.js
+++ src/index.js
@@ -1,7 +1,14 @@
 require('source-map-support').install()
 
+import R from 'ramda'
 import path from 'path'
 import sourcegate from 'sourcegate'
+
+export const pkg = require(path.join(process.cwd(), 'package.json'))
+
+export function gulpIsHelpful(gulp) {
+  return R.is(Object, R.path(['help', 'help'], gulp.tasks))
+}
 
 export function pollen(anthers, where) {
   let flaments = require(where || path.normalize('../src/pollen.json'))
