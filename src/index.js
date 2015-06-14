9750657231d557bd3660bc4b9eb50f0fa82573e6
Orlin M Bozhinov <om@soundsapiens.com>
2015-06-14 10:57:47 +0300

    harp's pollen -- relocated to gulp-harp

--- src/index.js
+++ src/index.js
@@ -2,21 +2,11 @@ require('source-map-support').install()
 
 import path from 'path'
 import sourcegate from 'sourcegate'
-let flaments = require(path.normalize('../src/pollen.json'))
 
-export function pollen(anthers) {
+export function pollen(anthers, where) {
+  let flaments = require(where || path.normalize('../src/pollen.json'))
   let got = anthers.map(select => {
     return typeof select === 'string' ? flaments[select] : select // object assumed
   })
   return sourcegate(got)
-}
-
-export function pollinateHarp(o) {
-  if (!o.harp) return {}
-  // infer what pollen is wanted
-  let anthers = ['harp']
-  if (o.harp.sync) anthers.push('harp-sync')
-  anthers.push(o)
-  // harp options
-  return pollen(anthers)
 }
