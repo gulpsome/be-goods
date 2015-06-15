0e3217c7020986324839ab39a7c1bf13194678b0
Orlin M Bozhinov <om@soundsapiens.com>
2015-06-15 19:25:17 +0300

    a helpful gulpTask

--- src/index.js
+++ src/index.js
@@ -10,6 +10,12 @@ export function gulpIsHelpful(gulp) {
   return R.is(Object, R.path(['help', 'help'], gulp.tasks))
 }
 
+// Helpful task creation.  The given help is discarded if gulp isn't gulp-help "helpful".
+export function gulpTask(gulp, name, help, ...rest) {
+  let args = (gulpIsHelpful(gulp)) ? [].concat(name, help, rest) : [].concat(name, rest)
+  return gulp.task.apply(gulp, args)
+}
+
 export function pollen(anthers, where) {
   let flaments = require(where || path.normalize('../src/pollen.json'))
   let got = anthers.map(select => {
