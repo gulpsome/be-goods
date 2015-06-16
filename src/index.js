cc5ce6ab3c30bd796d95ee56376f3df094199a0d
Orlin M Bozhinov <om@soundsapiens.com>
2015-06-16 17:51:18 +0300

    bypass explicit apply with es6

--- src/index.js
+++ src/index.js
@@ -18,7 +18,7 @@ export function gulpHelpify(gulp, opts) {
 // Helpful task creation.  The given desc is discarded if gulp isn't gulp-help "helpful".
 export function gulpTask(gulp, name, desc, ...rest) {
   let args = (gulpIsHelpful(gulp)) ? [].concat(name, desc, rest) : [].concat(name, rest)
-  return gulp.task.apply(gulp, args)
+  return gulp.task(...args)
 }
 
 export function pollen(anthers, where) {
