0dfd1fbf39cf4420b3071871ef515971aa1959e4
Orlin M Bozhinov <om@soundsapiens.com>
2016-05-02 16:42:56 +0300

    improved myRequire and moved the beverage-dependent req here to be-goods

--- index.js
+++ index.js
@@ -9,31 +9,47 @@ import tracer from 'tracer'
 
 export const pkg = require(path.join(process.cwd(), 'package.json'))
 
-export function isLocal(name) {
-  let dep = R.has(name)
-  return dep(pkg.dependencies || {}) || dep(pkg.devDependencies || {})
-}
-
-export function myRequire(name) {
-  let where = path.normalize(`${process.cwd()}/node_modules/${name}`)
-  return require(path.join(where, require(path.join(where, 'package.json')).main))
-}
-
 export let logger = tracer.console({
   'filters': {'log': chalk.green, 'warn': chalk.yellow, 'error': chalk.red},
   'format': `<${pkg.name} using {{path}}:{{line}}>\n{{message}}\n`
 })
 
-export function gulpIsHelpful(gulp) {
+export function isLocal (name) {
+  let dep = R.has(name)
+  return dep(pkg.dependencies || {}) || dep(pkg.devDependencies || {})
+}
+
+export function myRequire (name, home = '') {
+  let place = `${home}/node_modules/${name}`
+  let where = path.normalize(`${process.cwd()}/${place}`)
+  let main = require(path.join(where, 'package.json')).main
+  // console.log(`require ${place}`)
+  return require(path.join(where, main))
+}
+
+export function req (name) {
+  if (isLocal(name)) {
+    // local means relative to `process.cwd()`
+    return myRequire(name)
+  } else {
+    if (R.not(R.contains(name, ['hal-rc', 'gulp-cause', 'gulp-npm-run']))) {
+      // the above list of exceptions contains modules that will remain bundled as beverage dependencies
+      logger.error(`Please install ${name} as a devDependency.`)
+    }
+    return myRequire(name, 'node_modules/beverage')
+  }
+}
+
+export function gulpIsHelpful (gulp) {
   return R.is(Object, R.path(['help', 'help'], gulp.tasks))
 }
 
-export function gulpHelpify(gulp, opts) {
+export function gulpHelpify (gulp, opts) {
   return gulpIsHelpful(gulp) ? gulp : help(gulp, opts)
 }
 
 // Helpful task creation.  The given desc is discarded if gulp isn't gulp-help "helpful".
-export function gulpTask(gulp, name, desc, ...rest) {
+export function gulpTask (gulp, name, desc, ...rest) {
   let args = (gulpIsHelpful(gulp)) ? [].concat(name, desc, rest) : [].concat(name, rest)
   return gulp.task(...args)
 }
@@ -41,7 +57,7 @@ export function gulpTask(gulp, name, desc, ...rest) {
 // See https://github.com/gulpsome/gulp-harp about how to pollinate options.
 // Originally the defaults were here in pollen.json but that felt wrong and got moved.
 // Since there are no other use cases for this so far, it doesn't seem very useful.
-export function pollen(anthers, where) {
+export function pollen (anthers, where) {
   let flaments = require(where || path.normalize('pollen.json'))
   let got = anthers.map(select => {
     return typeof select === 'string' ? flaments[select] : select // object assumed
